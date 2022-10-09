1ed2af8b432c0ca923a607fae2cc7501e55210a9
Alexey Grunskii <you@example.com>
2022-10-09 23:03:10 +0300

    First commit

---
+++ asterisks-solid-square.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/env perl
+#===============================================================================
+#
+#         FILE: asterisks-solid-square.pl
+#
+#        USAGE: ./asterisks-solid-square.pl  
+#
+#  DESCRIPTION:1) Input
+#  DESCRIPTION:2)
+#  DESCRIPTION:3)
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: Alexey Grunskii (), robochat@mail.ru
+# ORGANIZATION: 
+#      VERSION: 1.0
+#      CREATED: 09.10.2022 13:21:44
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+use utf8;
+
+die 'Must be less 2 parameters' if (@ARGV>1);
+
+my $n=0;
+if (@ARGV==1) {
+	die 'N must be >= 0' if ($ARGV[0]<0);
+	$n=$ARGV[0];
+}
+
+for (my $i=0; $i<$n; $i++) {
+	for (my $j=0; $j<$n; $j++) {
+		print '*';
+	}
+	print "\n";
+}
